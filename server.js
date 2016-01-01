d2c35674990387ee70cdbd1d6e2b40b8c9989fd3
axelofan <cabanov.alexey@yandex.ru>
2016-01-01 14:48:08 +0400

    Fixed bugs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var app=require('express')();
 app.get('/', function(req,res){
 	res.sendfile('index.html');
 });
+app.get(/^(.+)$/, function(req, res){ 
+    res.sendfile( __dirname + req.params[0]); 
+ });
 app.listen(80);
 
 var piece=[];
