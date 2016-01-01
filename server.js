0e1c5f3ace98cae898e63e32cd43c8ea33745106
axelofan <cabanov.alexey@yandex.ru>
2016-01-01 14:18:50 +0400

    Fixed bugs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-require('fs').readFile('./index.html',function(data,err){
+require('fs').readFile('./index.html',function(err,data){
+	console.log(data);
 	require('http').createServer(function(req,res){
 		res.writeHead(200,{'Content-Type':'text/html'});
 		res.write(data);
 		res.end();
-	}).listen(8080);
+	}).listen(80);
 });
 
 var piece=[];
