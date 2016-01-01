9e8182b29fb3a2bd176c7a6ce1e633f5b1b16255
axelofan <cabanov.alexey@yandex.ru>
2016-01-01 16:14:40 +0400

    Just a bug...

--- forhtml.js
+++ forhtml.js
@@ -1,6 +1,7 @@
 ﻿var throttleTime=30, currentTime=Date.now();
 img = document.getElementById('img');
 function checkPiece(id){
+	console.log(id);
 	if (!$('#'+id).hasClass('piece')) return;
     if ((Math.abs($('#'+id).data('x')*colsWidth-$('#'+id).offset().left-$('#game').scrollLeft())<=3) && (Math.abs($('#'+id).data('y')*rowsHeight-$('#'+id).offset().top-$('#game').scrollTop())<=3)){
 		$('#'+id).draggable('disable').css({'top':$('#'+id).data('y')*rowsHeight, 'left':$('#'+id).data('x')*colsWidth,'z-index':1}).removeClass('piece').addClass('solved');
