948d921ced28b8ec811d5f1e7a695b8d630b26ce
axelofan <cabanov.alexey@yandex.ru>
2016-01-01 12:47:43 +0400

    v 0.1

---
+++ forhtml.js
@@ -0,0 +1,46 @@
+﻿img = document.getElementById('img');
+img.onload=function() { 
+    zIndex=3;
+    rows=8, cols=8;
+    rowsHeight = (img.height/rows);
+    colsWidth = (img.width/cols);
+    for(i=1; i<=rows; i++){
+        for(j=1; j<=cols; j++){
+			piece = document.createElement('canvas');
+			piece.id = 'piece'+i+'_'+j;
+			piece.className='piece';
+			piece.width=colsWidth;
+			piece.height=rowsHeight;
+			piece.style.zIndex=2;
+			piecectx=piece.getContext('2d');
+			piecectx.drawImage(img, (j-1)*colsWidth, (i-1)*rowsHeight, colsWidth, rowsHeight, 0, 0, colsWidth, rowsHeight);
+			document.getElementById('game').appendChild(piece);
+			$('#piece'+i+'_'+j).data({'x':j-1,'y':i-1});
+        }
+    }
+    img.height=500;
+    rowsHeight = Math.round(img.height/rows);
+    colsWidth = Math.round(img.width/cols);
+    $('.piece').css('width',colsWidth);
+	var socket=new WebSocket('ws://puzzle-axelofan.rhcloud.com:9000');
+	socket.onmessage=function(event) {
+		JSON.parse(event.data).forEach(function(item) {
+			$('#'+item.id).css({'left':item.left, 'top':item.top});
+			$('#'+item.id).mouseup();
+		});
+	}
+    $('.piece').draggable({drag: function() {
+		socket.send(JSON.stringify({'id':this.id,'left':$(this).css('left'),'top':$(this).css('top')}));
+	}});
+    $('.piece').mousedown(function(){
+        if (!$(this).hasClass('piece')) return;
+		$(this).css('z-index',zIndex); 
+        zIndex++;
+    });
+    $('.piece').mouseup(function(){
+        if (!$(this).hasClass('piece')) return;
+        if ((Math.abs($(this).data('x')*colsWidth-$(this).offset().left-$('#game').scrollLeft())<=3) && (Math.abs($(this).data('y')*rowsHeight-$(this).offset().top-$('#game').scrollTop())<=3)){
+			$(this).draggable('disable').css({'top':$(this).data('y')*rowsHeight, 'left':$(this).data('x')*colsWidth,'z-index':1}).removeClass('piece').addClass('solved');
+        }
+    });
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+var piece=[];
+for (i=1;i<=8;i++){
+	for (j=1;j<=8;j++){
+		piece.push({'id':'piece'+i+'_'+j,'left':Math.floor(Math.random()*400),'top':Math.floor(Math.random()*400)});
+	}
+}
+var wss=require('ws').Server({port:9000});
+wss.on('connection',function(ws) {
+	ws.send(JSON.stringify(piece));
+	ws.on('message', function(data) {
+		var a = JSON.parse(data);
+		for (i=0;i<piece.length;i++){
+			if(piece[i].id==a.id) {
+				piece[i].top=a.top;
+				piece[i].left=a.left;
+			}
+		}
+		//piece.forEach(function(item) {if(item.id==a.id){item.top=Math.floor(a.top);item.left=Math.floor(a.left);}});
+		data='['+data+']';
+		wss.clients.forEach(function(item) {if (item!=ws) item.send(data);});
+	});
+});
